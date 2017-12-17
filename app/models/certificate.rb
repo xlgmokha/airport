840c9021e5ee68f42a7471abdea3a8f409da541b
mo <mo.khan@gmail.com>
2017-12-17 12:44:48 -0700

    fix scope.

--- app/models/certificate.rb
+++ app/models/certificate.rb
@@ -1,5 +1,5 @@
 class Certificate < ApplicationRecord
   belongs_to :metadatum
   scope :signing, ->() { where(use: :signing) }
-  scope :encryption, ->() { where(use: :signing) }
+  scope :encryption, ->() { where(use: :encryption) }
 end
